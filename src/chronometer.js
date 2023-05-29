0a7d04f89c741e27ad26639df37a84668938e184
Ivan Silva <ivanjoaojr@hotmail.com>
2023-05-29 11:07:03 +0200

    first iteration, not yet final

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,58 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      printTimeCallback();
+    }, 1000)
   }
 
   getMinutes() {
-    // ... your code goes here
+    if (this.currentTime === 0) {
+      return 0
+    } else {
+      return Math.floor(this.currentTime / 60)
+    }
   }
 
   getSeconds() {
-    // ... your code goes here
+    if (this.currentTime === 0) {
+      return 0
+    } else if (this.currentTime < 60) {
+      return this.currentTime
+    } else {
+      let remainderSec = 0
+      remainderSec = this.currentTime % 60
+      return remainderSec
+    }
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value === 0) {
+      return '00'
+    } else {
+      let formattedValue = value.toString().padStart(2,'0')
+      return formattedValue
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId)
+
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    let minutes = this.computeTwoDigitNumber(this.getMinutes())
+    let seconds = this.computeTwoDigitNumber(this.getSeconds())
+    return `${minutes}:${seconds}`
   }
 }
+
